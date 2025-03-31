06e7cb2660646a1e9fe7a46455b0ee784df0bb86
henok3095 <henok3095@gmail.com>
2025-03-31 22:26:49 +0300

    commited

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    spotifyClientId: 'a6620f5c77e343cabe21a56dc5a0574', // Replace with your Spotify Client ID
+    spotifyClientId: 'a6620f5c77e343cabe21a56dc5a05746', // Replace with your Spotify Client ID
     redirectUri: 'https://zema-fm.vercel.app/callback',
     tokenExchangeUrl: 'https://your-backend-url.com/api/spotify/exchange-token' // Replace with your deployed backend URL (if applicable)
   };
